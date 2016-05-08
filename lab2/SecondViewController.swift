fa30e680991f885b5065fe6493db4492d81328db
Keith Choison <mickeychoison@sbcglobal.net>
2016-05-08 04:11:41 -0700

    Initial Commit

---
+++ lab2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  lab2
+//
+//  Created by Keith Choison on 5/8/16.
+//  Copyright © 2016 kchoison. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
